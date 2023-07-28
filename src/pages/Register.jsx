02899943e550cd1c43393c12b950abd60d91d5f8
CJacobe92 <jacobe.christian92@gmail.com>
2023-07-28 08:12:44 +0800

    fixed register routing bug

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,9 +2,10 @@ import React, { useContext, useState } from 'react'
 import RegisterForm from '../components/forms/RegisterForm'
 import { fetchRegister } from '../services/ApiFetch'
 import { UserContext } from '../reducers/UserContext'
+import { useNavigate } from 'react-router-dom'
 
 const Register = () => {
-
+  const navigate = useNavigate();
   const [request, setRequest] = useState({email: null, password: null, password_confirmation: null})
 
     const {dispatch} = useContext(UserContext)
@@ -21,7 +22,7 @@ const Register = () => {
 
         dispatch({type: 'SET_UID', payload: response.uid})
         dispatch({type: 'SET_AUTH', payload: response.auth})
-
+        navigate('/')
     }
   return (
     <div className='min-h-screen w-[100%] flex justify-center items-center'>
